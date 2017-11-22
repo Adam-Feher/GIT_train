5f22fa16e9164f57279b4fd7ca7e98b51406d94b
Daniel Császár <daniel.cs20@gmail.com>
2017-11-22 09:27:00 +0100

    Added very bad joke method.

--- gittie.py
+++ gittie.py
@@ -2,3 +2,6 @@ class should_not_be_this_fancy:
 
     def introduce(self):
         print("HELLO I AM GITTY!! :D :D")
+
+    def joke(self):
+        print("WHY DOES PETIE DOES NOT COME DOWN TO PLAY? HE HAS NO LEGS HAHAHAHAH")
